90ffc40be44f691891631b0d1b329ff67227fb48
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-05-07 21:05:28 +0200

    uprava a funkční úkol task 1 strategie

--- main.py
+++ main.py
@@ -20,19 +20,19 @@ class MinStrategy(OperatorFile):
 
 class MaxStrategy(OperatorFile):
     def sum(self, my_num: List[int]) -> float:
-        return sum(my_num) * 0.9
+        return max(my_num)
 
 # File Operator (Context)
 class CashDesk:
     def __init__(self):
         self.my_num = []
-        self.strategy = NormalStrategy()
+        self.strategy = MinStrategy()
 
     def add(self, price):
         self.my_num.append(price)
 
     def print_bill(self):
-        total_price = self.strategy.sum(self.drinks)
+        total_price = self.strategy.sum(self.my_num)
         print(f"Celkem k zaplaceni: {total_price}")
         print("Dekujeme za navstevu, vratte se znovu!")
         self.my_num = []
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     cash_desk = CashDesk()
 
     # Happy Hour
-    cash_desk.set_strategy(HappyHourStrategy())
+    cash_desk.set_strategy(MinStrategy())
     cash_desk.add(20)
     cash_desk.add(80)
     cash_desk.print_bill()
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     # Koniec Happy Hour
 
     # Normálna stratégia
-    cash_desk.set_strategy(NormalStrategy())
+    cash_desk.set_strategy(MaxStrategy())
     cash_desk.add(20)
     cash_desk.add(80)
     cash_desk.print_bill()
